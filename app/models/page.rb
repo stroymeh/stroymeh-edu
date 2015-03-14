a8cece232bd9be9ec2a1a956c94561d40f178606
tab <tukhfatullin@gmail.com>
2015-03-14 22:18:17 +0600

    Add friendly_id for pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, :use => [:slugged, :finders]
+
   belongs_to :section
 
   validates_presence_of :title, :content, :tags, :section_id

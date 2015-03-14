1d9f0e7bfb433fe75934c32ea060a965ac24adbb
tab <tukhfatullin@gmail.com>
2015-03-14 21:51:07 +0600

    Fix page titles

--- config/manage_navigation.rb
+++ config/manage_navigation.rb
@@ -2,15 +2,15 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
 
   navigation.items do |primary|
-    primary.item :section, I18n.t('page_title.section.index'), manage_root_path do |section|
+    primary.item :section, I18n.t('page_title.sections.index'), manage_root_path do |section|
       if @section && @section.persisted?
-        section.item :edit,  I18n.t('page_title.section.edit', :title => @section),  edit_manage_section_path(@section)
+        section.item :edit,  I18n.t('page_title.sections.edit', :title => @section),  edit_manage_section_path(@section)
       end
-      section.item :new,   I18n.t('page_title.section.new'),   new_manage_section_path
+      section.item :new,   I18n.t('page_title.sections.new'),   new_manage_section_path
 
-      section.item :new, I18n.t('page_title.page.new'), new_manage_page_path
+      section.item :new, I18n.t('page_title.pages.new'), new_manage_page_path
       if @page && @page.persisted?
-        section.item :edit, I18n.t('page_title.page.edit'), edit_manage_page_path(@page)
+        section.item :edit, I18n.t('page_title.pages.edit'), edit_manage_page_path(@page)
       end
     end
   end

ca74645adf1ebc571d1c816bd1ec2d95a04b7d2f
tab <tukhfatullin@gmail.com>
2015-03-14 22:44:10 +0600

    Add auto_html gem for section description

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,17 @@
 class Section < ActiveRecord::Base
-  extend FriendlyId
-  friendly_id :title, :use => [:slugged, :finders]
-
   has_many :pages, :dependent => :destroy
 
   validates_presence_of :title, :description
 
   alias_attribute :to_s, :title
+
+  include AutoHtml
+  auto_html_for :description do
+    html_escape
+    image
+    simple_format
+  end
+
+  extend FriendlyId
+  friendly_id :title, :use => [:slugged, :finders]
 end

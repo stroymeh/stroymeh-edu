b97b5a6b9dba1808bbd5baf608d72538b881203c
tab <tukhfatullin@gmail.com>
2016-10-29 13:17:02 +0700

    Fix mail initializer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,12 @@
 unless Rails.env.test?
   ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         ENV['APP_HOST'],
-    :authentication => :plain
+    :port =>           ENV['SMTP_PORT'],
+    :address =>        ENV['SMTP_HOST'],
+    :user_name =>      ENV['SMTP_USERNAME'],
+    :password =>       ENV['SMPT_PASSWORD'],
+    :domain =>         ENV['SMTP_DOMAIN'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   ActionMailer::Base.delivery_method = :smtp
